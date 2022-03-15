3f4c610c831d75d73c89df05ab4800c76d7af414
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-15 20:56:16 +0300

    Modify blueprint paths

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,14 @@ def create_app():
 
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     # Initiliase Database
     db.init_app(app)
 
     # Register Blueprint
-    from main.auth import auth
-    from main.views import views
+    from .main.auth import auth
+    from .main.views import views
     app.register_blueprint(auth)
     app.register_blueprint(views)
 
