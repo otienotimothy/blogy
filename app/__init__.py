20e31e85abe687cb2169dffc5617c7285673a15a
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-15 16:30:04 +0300

    Add and register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,15 @@ def create_app():
 
     app = Flask(__name__)
 
+    # Initiliase Database
     db.init_app(app)
+
+    # Register Blueprint
+    from main.auth import auth
+    from main.views import views
+    app.register_blueprint(auth)
+    app.register_blueprint(views)
+
+    
 
     return app
