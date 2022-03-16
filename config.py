0c401852540e5de0249f8615391cae720ca8051a
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-16 12:34:40 +0300

    Modify configurations

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:moringa@localhost/blogy'
 
 
 class DevConfig(Config):
     '''
     Development Configs
     '''
-
+    ENV='development'
     DEBUG=True
 
 class ProdConfig(Config):
