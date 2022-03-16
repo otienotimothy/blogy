49e0a47961b431be2afe550d822872bf00c75981
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-16 18:57:01 +0300

    Add functionality to render signup template

--- app/main/auth.py
+++ app/main/auth.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
 from app.forms import CreateUserForm, LoginForm
 
-auth = Blueprint('auth', __name__, url_prefix='auth')
+auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@auth.route('/signup')
+def signup():
+
+    create_user = CreateUserForm()
+
+    return render_template('signup.html', form = create_user)
 
 @auth.route('/login')
 def login():
