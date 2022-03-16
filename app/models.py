610b5953ddaa2400add4526b2034e32c7bd25ad6
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-16 11:09:30 +0300

    Add a subscribe model

--- app/models.py
+++ app/models.py
@@ -62,3 +62,10 @@ class Like(db.Model):
         'users.id', ondelete='CASCADE'), nullable=False)
     liked_post = db.Column(db.Integer, db.ForeignKey(
         'post.id', ondelete='CASCADE'), nullable=False)
+
+class Subscribe(db.Model):
+
+    __tablename__ = 'subscribers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique = True, nullable = False)
