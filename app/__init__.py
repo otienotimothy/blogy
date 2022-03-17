e7a01d94efa61eace420f7badceff716df432475
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-17 09:36:49 +0300

    Add secret key

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     app = Flask(__name__)
 
     # app.config.from_object(config_options['development'])
-
+    app.config['SECRET_KEY'] = '599d43a5cfba1ced86ae18d25da9fc3ef8e9dacbccd3eaa338296ea3a8549cde'
     # Add App Configs
     if app.config['ENV'] == 'development':
         app.config.from_object(config_options['development'])
